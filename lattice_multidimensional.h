c4094370d5c565ac186c5d686c4584d9cad0c27c
Michal Sara <michal.sara99@gmail.com>
2020-06-26 14:04:08 +0200

    CHANGES: 1) Added suport for multidimensional lattices in lattice_multidimensional.h module

--- lattice_multidimensional.h
+++ lattice_multidimensional.h
@@ -3,24 +3,56 @@
 #define _LATTICE_MULTIDIMENSIONAL
 
 #include"lattice_types.h"
+#include"lattice_macros.h"
+#include"lattice_structure.h"
+#include"lattice_miscellaneous.h"
+#include<type_traits>
 #include<array>
 
 namespace lattice_multidimensional {
 
 	using lattice_types::LatticeType;
+	using lattice_structure::IndexedLattice;
+	using lattice_structure::MeanRevertingLattice;
+	using lattice_structure::Lattice;
+	using lattice_structure::MeanRevertingIndexedLattice;
+	using lattice_miscellaneous::MeanRevertingParams;
 
 	// ==============================================================================
 	// ============================= MultidimGeneralLattice =========================
 	// ==============================================================================
 
-	template<LatticeType Type,
+	template<std::size_t LatticeDimension,
+			LatticeType Type,
 			typename Node,
 			typename TimeAxis,
 			typename NodeContainerType,
-			template<LatticeType,typename...> typename ...LatticeObjects>
+			template<LatticeType,typename...> typename LatticeObject,
+			typename  = typename std::enable_if<(LatticeDimension > 1)>::type>
 	class MultidimGeneralLattice {
+	public:
+		virtual ~MultidimGeneralLattice(){}
+
+		std::size_t constexpr factors()const { return LatticeDimension; }
 	
-	
+	};
+
+	// ==============================================================================
+	// ====================== MultidimMeanRevertingGeneralLattice ===================
+	// ==============================================================================
+
+	template<std::size_t LatticeDimension,
+		typename Node,
+		typename TimeAxis,
+		typename NodeContainerType,
+		template<typename...> typename LatticeObject,
+		typename = typename std::enable_if<(LatticeDimension > 1)>::type>
+		class MultidimMeanRevertingGeneralLattice {
+		public:
+			virtual ~MultidimMeanRevertingGeneralLattice() {}
+
+			std::size_t constexpr factors()const { return LatticeDimension; }
+
 	};
 
 
@@ -29,13 +61,43 @@ namespace lattice_multidimensional {
 	// ==============================================================================
 
 
-	template<LatticeType Type,
-			typename Node,
-			template<LatticeType,typename> typename ...IndexLattices>
-	class MultidimIndexedLattice :public MultidimGeneralLattice<Type, Node, std::size_t, std::vector<Node>,IndexLattices ...> {
+	template<std::size_t Dimension,
+			LatticeType Type,
+			typename Node>
+	class MultidimIndexedLattice :public MultidimGeneralLattice<Dimension,Type, Node, std::size_t, std::vector<Node>, IndexedLattice> {
+	protected:
+		std::vector<IndexedLattice<Type, Node>> multiTree_;
 
+	public:
+		explicit MultidimIndexedLattice(std::size_t numberPeriods) {
+			for (std::size_t t = 0; t < Dimension; ++t)
+				multiTree_.emplace_back(std::move(IndexedLattice<Type, Node>{ numberPeriods }));
+		}
 
+		IndexedLattice<Type, Node> getFactor(std::size_t factorIdx) {
+			LASSERT(factorIdx < Dimension, "factor index is out of range");
+			return multiTree_[factorIdx];
+		}
 
+		Node const& operator()(std::size_t factorIdx, std::size_t timeIdx, std::size_t leafIdx)const {
+			LASSERT(factorIdx < Dimension, "factor index is out of range");
+			return multiTree_[factorIdx](timeIdx, leafIdx);
+		}
+
+		Node &operator()(std::size_t factorIdx, std::size_t timeIdx, std::size_t leafIdx) {
+			LASSERT(factorIdx < Dimension, "factor index is out of range");
+			return multiTree_[factorIdx](timeIdx, leafIdx);
+		}
+
+		Node const &at(std::size_t factorIdx, std::size_t timeIdx, std::size_t leafIdx)const {
+			LASSERT(factorIdx < Dimension, "factor index is out of range");
+			return multiTree_[factorIdx](timeIdx, leafIdx);
+		}
+
+		Node apex(std::size_t factorIdx)const {
+			LASSERT(factorIdx < Dimension, "factor index is out of range");
+			return multiTree_[factorIdx].apex();
+		}
 	};
 
 
@@ -43,30 +105,148 @@ namespace lattice_multidimensional {
 	// =================================== MultidimLattice ==========================
 	// ==============================================================================
 	 
-	template<LatticeType Type,
+	template<std::size_t Dimension,
+		LatticeType Type,
 		typename Node,
-		typename TimeAxis,
-		template<LatticeType, typename, typename> typename ...Lattices>
-	class MultidimLattice :public MultidimGeneralLattice<Type, Node, TimeAxis, std::vector<Node>,Lattices ...> {
+		typename TimeAxis>
+	class MultidimLattice :public MultidimGeneralLattice<Dimension,Type, Node, TimeAxis, std::vector<Node>,Lattice> {
+	protected:
+		std::vector<Lattice<Type, Node, TimeAxis>> multiTree_;
 
+	public:
+		explicit MultidimLattice(std::set<TimeAxis> const &fixingDatesSet) {
+			for (std::size_t t = 0; t < Dimension; ++t)
+				multiTree_.emplace_back(std::move(Lattice<Type, Node, TimeAxis>{ fixingDatesSet }));
+		}
+
+		explicit MultidimLattice(std::initializer_list<TimeAxis> const &fixingDates) {
+			for (std::size_t t = 0; t < Dimension; ++t)
+				multiTree_.emplace_back(std::move(Lattice<Type, Node, TimeAxis>{ fixingDates }));
+		}
+
+		Lattice<Type, Node, TimeAxis> getFactor(std::size_t factorIdx) {
+			LASSERT(factorIdx < Dimension, "factor index is out of range");
+			return multiTree_[factorIdx];
+		}
+
+		Node const& operator()(std::size_t factorIdx, std::size_t timeIdx, std::size_t leafIdx)const {
+			LASSERT(factorIdx < Dimension, "factor index is out of range");
+			return multiTree_[factorIdx](timeIdx, leafIdx);
+		}
+
+		Node &operator()(std::size_t factorIdx, std::size_t timeIdx, std::size_t leafIdx) {
+			LASSERT(factorIdx < Dimension, "factor index is out of range");
+			return multiTree_[factorIdx](timeIdx, leafIdx);
+		}
+
+		Node const &at(std::size_t factorIdx, std::size_t timeIdx, std::size_t leafIdx)const {
+			LASSERT(factorIdx < Dimension, "factor index is out of range");
+			return multiTree_[factorIdx](timeIdx, leafIdx);
+		}
+
+		Node apex(std::size_t factorIdx)const {
+			LASSERT(factorIdx < Dimension, "factor index is out of range");
+			return multiTree_[factorIdx].apex();
+		}
 
 
 	};
 
 
+	// ==============================================================================
+	// ===================== MultidimMeanRevertingIndexedLattice ====================
+	// ==============================================================================
+
+
+	template<std::size_t Dimension,
+		typename Node>
+		class MultidimMeanRevertingIndexedLattice :public MultidimMeanRevertingGeneralLattice<Dimension,Node, std::size_t,
+												std::vector<Node>, MeanRevertingIndexedLattice> {
+		protected:
+			std::vector<MeanRevertingIndexedLattice<Node>> multiTree_;
+
+		public:
+			template<typename DeltaTime>
+			explicit MultidimMeanRevertingIndexedLattice(std::size_t numberPeriods, MeanRevertingParams<Node> const &params, DeltaTime const &deltaTime) {
+				for (std::size_t t = 0; t < Dimension; ++t)
+					multiTree_.emplace_back(std::move(MeanRevertingIndexedLattice<Node>{ numberPeriods,params,deltaTime }));
+			}
+
+			MeanRevertingIndexedLattice<Node> getFactor(std::size_t factorIdx) {
+				LASSERT(factorIdx < Dimension, "factor index is out of range");
+				return multiTree_[factorIdx];
+			}
+
+			Node const& operator()(std::size_t factorIdx, std::size_t timeIdx, std::size_t leafIdx)const {
+				LASSERT(factorIdx < Dimension, "factor index is out of range");
+				return multiTree_[factorIdx](timeIdx, leafIdx);
+			}
+
+			Node &operator()(std::size_t factorIdx, std::size_t timeIdx, std::size_t leafIdx) {
+				LASSERT(factorIdx < Dimension, "factor index is out of range");
+				return multiTree_[factorIdx](timeIdx, leafIdx);
+			}
+
+			Node const &at(std::size_t factorIdx, std::size_t timeIdx, std::size_t leafIdx)const {
+				LASSERT(factorIdx < Dimension, "factor index is out of range");
+				return multiTree_[factorIdx](timeIdx, leafIdx);
+			}
+
+			Node apex(std::size_t factorIdx)const {
+				LASSERT(factorIdx < Dimension, "factor index is out of range");
+				return multiTree_[factorIdx].apex();
+			}
+	};
+
+
+	// ==============================================================================
+	// ===================== MultidimMeanRevertingLattice ===========================
+	// ==============================================================================
+
+
+	template<std::size_t Dimension,
+		typename Node,
+		typename TimeAxis>
+		class MultidimMeanRevertingLattice :public MultidimMeanRevertingGeneralLattice<Dimension, Node, TimeAxis,
+		std::vector<Node>, MeanRevertingLattice> {
+		protected:
+			std::vector<MeanRevertingLattice<Node,TimeAxis>> multiTree_;
+
+		public:
+			template<typename DeltaTime>
+			explicit MultidimMeanRevertingLattice(std::set<TimeAxis> const &fixingDatesSet, MeanRevertingParams<Node> const &params, DeltaTime const &deltaTime) {
+				for (std::size_t t = 0; t < Dimension; ++t)
+					multiTree_.emplace_back(std::move(MeanRevertingLattice<Node,TimeAxis>{ numberPeriods, params, deltaTime }));
+			}
+
+			MeanRevertingLattice<Node, TimeAxis> getFactor(std::size_t factorIdx) {
+				LASSERT(factorIdx < Dimension, "factor index is out of range");
+				return multiTree_[factorIdx];
+			}
+
+			Node const& operator()(std::size_t factorIdx, std::size_t timeIdx, std::size_t leafIdx)const {
+				LASSERT(factorIdx < Dimension, "factor index is out of range");
+				return multiTree_[factorIdx](timeIdx, leafIdx);
+			}
+
+			Node &operator()(std::size_t factorIdx, std::size_t timeIdx, std::size_t leafIdx) {
+				LASSERT(factorIdx < Dimension, "factor index is out of range");
+				return multiTree_[factorIdx](timeIdx, leafIdx);
+			}
+
+			Node const &at(std::size_t factorIdx, std::size_t timeIdx, std::size_t leafIdx)const {
+				LASSERT(factorIdx < Dimension, "factor index is out of range");
+				return multiTree_[factorIdx](timeIdx, leafIdx);
+			}
+
+			Node apex(std::size_t factorIdx)const {
+				LASSERT(factorIdx < Dimension, "factor index is out of range");
+				return multiTree_[factorIdx].apex();
+			}
+	};
 
 
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
--- lattice_multidimensional_t.h
+++ lattice_multidimensional_t.h
@@ -6,7 +6,7 @@
 #include"lattice_types.h"
 #include"lattice_structure.h"
 #include"lattice_multidimensional.h"
-
+#include"lattice_utility.h"
 
 void testCreate2DIndexedLattice(){
 
@@ -14,15 +14,28 @@ void testCreate2DIndexedLattice(){
 	using lattice_multidimensional::MultidimIndexedLattice;
 	using lattice_structure::IndexedLattice;
 	using lattice_types::LatticeType;
+	using lattice_utility::print;
+
+	std::size_t periods = 5;
+
+	MultidimIndexedLattice<2,LatticeType::Binomial, double> index2Dtree(periods);
+
+	std::cout << "Number of factors: " << index2Dtree.factors() << "\n";
+	index2Dtree(0, 0, 0) = 3.1415;
+	index2Dtree(1, 1, 0) = std::exp(1.0);
+
+	auto firstFactor = index2Dtree.getFactor(0);
+	auto secondFactor = index2Dtree.getFactor(1);
+
+	std::cout << "Print first factor: \n";
+	print(firstFactor, firstFactor.begin(), firstFactor.end());
+	std::cout << "\nPrint second factor: \n";
+	print(secondFactor, secondFactor.begin(), secondFactor.end());
 
 
-	MultidimIndexedLattice<LatticeType::Binomial, double, IndexedLattice, IndexedLattice> index2Dtree;
-
-
-
-
-
-
+	std::cout << "My apex: \n";
+	std::cout << "1: " << index2Dtree.apex(0) << "\n";
+	std::cout << "2: " << index2Dtree.apex(1) << "\n";
 
 }
 
